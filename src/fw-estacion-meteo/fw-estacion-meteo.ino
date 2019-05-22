afa61ee4c03bd26c945c75ffe0f5df319dec4c11
Pablo Bacho <pablo@fabricadigital.org>
2019-05-22 17:58:11 +0200

    Programa pantalla

--- src/fw-estacion-meteo/fw-estacion-meteo.ino
+++ src/fw-estacion-meteo/fw-estacion-meteo.ino
@@ -1,7 +1,19 @@
+#include <Adafruit_SPITFT_Macros.h>
+#include <Adafruit_GFX.h>
+#include <gfxfont.h>
+#include <Adafruit_SPITFT.h>
+#include <Adafruit_ST7789.h>
+#include <Adafruit_ST77xx.h>
+#include <Adafruit_ST7735.h>
+#include <Fonts/FreeSans9pt7b.h>
+#include <Fonts/FreeSans24pt7b.h>
+
 #include <Adafruit_BME280.h>
 
-// Variables sensor de aire
+// Controlador de la pantalla
+Adafruit_ST7735 pantalla = Adafruit_ST7735(10, 8, 9);
 
+// Variables sensor de aire
 Adafruit_BME280 bme280; // Crear objeto que controla el sensor
 double temperaturaAire; // Crear variable para guardar la temperatura del aire
 double humedadAire; // Crear variable para guardar la humedad del aire
@@ -26,7 +38,21 @@ const int ANEMOMETRO_RADIO = 99; // en milímetros
 const double ANEMOMETRO_PERIMETRO = 2 * 3.141592 * ANEMOMETRO_RADIO;
 int velocidadDelViento;
 
+// Variables del botón
+const int BOTON_PIN = 5;
+int estadoActualBoton;
+int estadoAnteriorBoton;
 
+// Variables del estado de la pantalla
+const int PANTALLA_TEMPERATURA = 0;
+const int PANTALLA_HUMEDAD = 1;
+const int PANTALLA_PRESION = 2;
+const int PANTALLA_VELOCIDADVIENTO = 3;
+int estadoPantalla = PANTALLA_TEMPERATURA;
+
+// Variables para medir el tiempo y refrescar la pantalla
+unsigned long int tiempoUltimaActualizacion;
+const unsigned long int TIEMPO_ENTRE_ACTUALIZACIONES = 20000;
 
 void imprimirTemperaturaAire() {
   Serial.print("Temperatura: "); // Imprimir el valor de la temperatura del aire con 1 decimal
@@ -78,22 +104,109 @@ void anemometroMedir() {
   anemometro_nivelLaserAnterior = anemometro_nivelLaser; // Guardar el nivel del láser para compararlo con la próxima lectura
 }
 
+void actualizarDatos() {
+  switch(estadoPantalla) {
+    case 0:
+      temperaturaAire = bme280.readTemperature(); // Leer temperatura del aire y guardarla en su variable
+      imprimirTemperaturaAire();
+      pantalla.fillScreen(ST7735_YELLOW); // Limpiar la pantalla con fondo amarillo
+      pantalla.setTextColor(ST7735_BLACK);// Seleccionar el color del texto
+      pantalla.setFont(&FreeSans9pt7b);  // Seleccionar la fuente pequeña
+      pantalla.setCursor(20, 20);
+      pantalla.print("Temperatura");
+      pantalla.setFont(&FreeSans24pt7b);  // Seleccionar la fuente grande
+      pantalla.setCursor(10, 100);
+      pantalla.print(temperaturaAire, 1);
+      pantalla.setFont(&FreeSans9pt7b);  // Seleccionar la fuente pequeña
+      pantalla.print(" C");
+      break;
+    case 1:
+      humedadAire = bme280.readHumidity(); // Leer humedad relativa del aire
+      imprimirHumedadAire();
+      pantalla.fillScreen(ST7735_BLUE); // Limpiar la pantalla con fondo amarillo
+      pantalla.setFont(&FreeSans9pt7b);  // Seleccionar la fuente pequeña
+      pantalla.setTextColor(ST7735_WHITE);// Seleccionar el color del texto
+      pantalla.setCursor(20, 50);
+      pantalla.print("Humedad");
+      pantalla.setFont(&FreeSans24pt7b);  // Seleccionar la fuente grande
+      pantalla.setCursor(10, 100);
+      pantalla.print(humedadAire, 0);
+      pantalla.setFont(&FreeSans9pt7b);  // Seleccionar la fuente pequeña
+      pantalla.print(" %");
+      break;
+    case 2:
+      presionBarometrica = bme280.readPressure() / 100; // Leer presión barométrica y convertirla a milibares
+      imprimirPresionBarometrica();
+      pantalla.fillScreen(ST7735_RED); // Limpiar la pantalla con fondo amarillo
+      pantalla.setFont(&FreeSans9pt7b);  // Seleccionar la fuente pequeña
+      pantalla.setTextColor(ST7735_WHITE);// Seleccionar el color del texto
+      pantalla.setCursor(20, 50);
+      pantalla.print("Presion");
+      pantalla.setFont(&FreeSans24pt7b);  // Seleccionar la fuente grande
+      pantalla.setCursor(10, 100);
+      pantalla.print(presionBarometrica, 0);
+      pantalla.setFont(&FreeSans9pt7b);  // Seleccionar la fuente pequeña
+      pantalla.print(" mbar"); // Es lo mismo que hPa
+      break;
+    case 3:
+      imprimirVelocidadDelViento();
+      pantalla.fillScreen(ST7735_CYAN); // Limpiar la pantalla con fondo amarillo
+      pantalla.setFont(&FreeSans9pt7b);  // Seleccionar la fuente pequeña
+      pantalla.setTextColor(ST7735_BLACK);// Seleccionar el color del texto
+      pantalla.setCursor(20, 50);
+      pantalla.print("Viento");
+      pantalla.setFont(&FreeSans24pt7b);  // Seleccionar la fuente grande
+      pantalla.setCursor(10, 100);
+      pantalla.print(velocidadDelViento, DEC);
+      pantalla.setFont(&FreeSans9pt7b);  // Seleccionar la fuente pequeña
+      pantalla.print(" km/h"); // Es lo mismo que hPa
+      break;
+  }
+
+  tiempoUltimaActualizacion = millis(); // Actualiza el tiempo del último refresco de pantalla.
+}
+
 void setup() {
   bme280.begin(0x76); // Inicializar el sensor que tiene la dirección I²C 0x76
   Serial.begin(9600); // Inicializar puerto serie
+
+  pinMode(BOTON_PIN, INPUT_PULLUP); // Configurar pin del botón como entrada con pullup
+  estadoActualBoton = digitalRead(BOTON_PIN); // Leer el estado inicial del botón
+  estadoAnteriorBoton = estadoActualBoton;
+
+  // Inicializar pantalla
+  pantalla.initR(INITR_BLACKTAB);
+  // Rotar pantalla para ponerla apaisada
+  pantalla.setRotation(1);
+  // Limpiar pantalla (rellenar de negro)
+  pantalla.fillScreen(ST7735_BLACK);
+
+  pantalla.setFont(&FreeSans9pt7b);
+  pantalla.setTextWrap(false);
+  pantalla.setCursor(17, 50);
+  pantalla.print("Estacion Meteo");
+  pantalla.setCursor(14, 70);
+  pantalla.print("por Pablo Bacho");
+
+  delay(2000);
+  actualizarDatos();
 }
 
 void loop() {
-  temperaturaAire = bme280.readTemperature(); // Leer temperatura del aire y guardarla en su variable
-  humedadAire = bme280.readHumidity(); // Leer humedad relativa del aire
-  presionBarometrica = bme280.readPressure() / 100; // Leer presión barométrica y convertirla a milibares
   anemometroMedir();
+
+  estadoActualBoton = digitalRead(BOTON_PIN);
+  if(estadoActualBoton == LOW && estadoAnteriorBoton == HIGH) {
+    estadoPantalla++;
+    if(estadoPantalla > 3) {
+      estadoPantalla = 0;
+    }
+    actualizarDatos();
+  }
+  estadoAnteriorBoton = estadoActualBoton; // Guardar el estado del botón para la próxima vuelta
+
+  if(millis() - tiempoUltimaActualizacion >= TIEMPO_ENTRE_ACTUALIZACIONES) {
+    actualizarDatos();
+  }
   
-  imprimirTemperaturaAire();
-  imprimirHumedadAire();
-  imprimirPresionBarometrica();
-  imprimirVelocidadDelViento();
-
-  Serial.println();
-
 }
